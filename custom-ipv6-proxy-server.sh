940226de686cb389b3121c81647134a650487156
2002-115 <85139299+2002-115@users.noreply.github.com>
2024-07-15 11:00:23 +0700

    Update custom-ipv6-proxy-server.sh

--- custom-ipv6-proxy-server.sh
+++ custom-ipv6-proxy-server.sh
@@ -6,8 +6,12 @@ sudo su
 # Download the ipv6-proxy-server script
 wget https://raw.githubusercontent.com/Temporalitas/ipv6-proxy-server/master/ipv6-proxy-server.sh && chmod +x ipv6-proxy-server.sh
 
-# Run the ipv6-proxy-server script with placeholder credentials
-./ipv6-proxy-server.sh -s 64 -c 100 -u placeholder -p placeholder -t http -r 10
+# Generate random username and password
+USERNAME=$(openssl rand -base64 12)
+PASSWORD=$(openssl rand -base64 12)
+
+# Run the ipv6-proxy-server script with generated credentials
+./ipv6-proxy-server.sh -s 64 -c 100 -u $USERNAME -p $PASSWORD -t http -r 10
 
 # Check if the proxy list file exists
 PROXY_FILE="/root/proxyserver/backconnect_proxies.list"
@@ -24,19 +28,17 @@ if [ -f "$PROXY_FILE" ]; then
         echo "$IP_PORT:$USERNAME:$PASSWORD" >> $UNIQUE_PROXY_FILE
     done < $PROXY_FILE
 
-    # Create a zip file with a random 6-character password
-    ZIP_FILE="/root/proxyserver/backconnect_proxies.zip"
-    ZIP_PASSWORD=$(openssl rand -base64 6 | cut -c1-6)
-    zip -P $ZIP_PASSWORD $ZIP_FILE $UNIQUE_PROXY_FILE
+    # Create a tar.gz file with a random 6-character password
+    TAR_FILE="/root/proxyserver/backconnect_proxies.tar.gz"
+    tar czf $TAR_FILE $UNIQUE_PROXY_FILE
 
-    # Upload the zip file to Bashupload
-    UPLOAD_RESPONSE=$(curl -s --upload-file $ZIP_FILE https://bashupload.com/$ZIP_FILE)
+    # Upload the tar.gz file to Bashupload
+    UPLOAD_RESPONSE=$(curl -s --upload-file $TAR_FILE https://bashupload.com/$TAR_FILE)
 
     # Extract the download link from the response
     DOWNLOAD_LINK=$(echo $UPLOAD_RESPONSE | grep -o 'https://bashupload.com/[^ ]*')
 
-    echo "You can download the zipped proxy list from: $DOWNLOAD_LINK"
-    echo "The password for the zip file is: $ZIP_PASSWORD"
+    echo "You can download the tar.gz proxy list from: $DOWNLOAD_LINK"
 else
     echo "Proxy list file not found!"
 fi
