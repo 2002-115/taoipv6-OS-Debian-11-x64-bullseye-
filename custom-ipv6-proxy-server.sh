670add8bfdd09b9fa4a7b715e929ea46f71e24d1
2002-115 <85139299+2002-115@users.noreply.github.com>
2024-07-16 09:00:23 +0700

    Update custom-ipv6-proxy-server.sh

--- custom-ipv6-proxy-server.sh
+++ custom-ipv6-proxy-server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Xóa proxy cũ
+
+# Xóa proxy cũ nếu có
 ./ipv6-proxy-server.sh --uninstall
 
-# Elevate to root
+# Elevate to root (nếu chưa)
 sudo su
 
-# Tải xuống script ipv6-proxy-server
+# Cài đặt các gói cần thiết
+sudo apt-get update
+sudo apt-get install wget zip curl openssl gcc make git -y
+
+# Tải xuống script ipv6-proxy-server nếu chưa có sẵn
 wget http://69.28.88.79/tool%20ipv6/ipv6-proxy-server.sh && chmod +x ipv6-proxy-server.sh
 
 # Tạo username và password ngẫu nhiên không chứa dấu "+"
@@ -34,8 +39,39 @@ if [ -f "$PROXY_FILE" ]; then
     # Trích xuất link tải về từ phản hồi
     DOWNLOAD_LINK=$(echo $UPLOAD_RESPONSE | grep -o 'https://bashupload.com/[^ ]*')
 
-    echo "Bạn có thể tải danh sách proxy đã nén từ: $DOWNLOAD_LINK"
+    echo "Bạn có thể tải danh sách proxy đã nén
+    từ: $DOWNLOAD_LINK"
     echo "Mật khẩu cho file zip là: $ZIP_PASSWORD"
 else
     echo "Không tìm thấy file danh sách proxy!"
 fi
+
+# Tải và biên dịch 3proxy từ mã nguồn nếu chưa có sẵn 
+git clone https://github.com/z3APA3A/3proxy.git || true # ignore if already exists 
+cd 3proxy 
+make -f Makefile.Linux 
+sudo make -f Makefile.Linux install 
+
+# Cấu hình và khởi động 3proxy 
+sudo mkdir /etc/3proxy || true # ignore if already exists 
+sudo bash -c "cat > /etc/3proxy/3proxy.cfg <<EOF 
+nserver 8.8.8.8 
+nserver 8.8.4.4 
+nscache 65536 
+
+config /conf/3proxy.cfg 
+
+monitor /conf/3proxy.cfg 
+
+log /var/log/3proxy.log D 
+rotate 30 
+
+auth strong 
+users $USERNAME:CL:$PASSWORD 
+
+allow * * * * 
+
+proxy -6 -n -a -p10000-10099 -i::1 -e::1 
+EOF"
+
+sudo systemctl restart 3proxy.service
